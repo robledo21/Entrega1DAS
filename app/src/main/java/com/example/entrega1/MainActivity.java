0f5d977e9ccf97da6c52d490d4a5958fbd83806e
robledo21 <jonrobledolopez@gmail.com>
2024-02-25 10:39:22 +0100

    Primera subida

---
+++ app/src/main/java/com/example/entrega1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.entrega1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        setContentView(R.layout.activity_main);
+    }
+}
