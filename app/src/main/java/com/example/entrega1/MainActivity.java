5bf1b050a8579d015961d7cfc43ba7a3500be0a9
robledo21 <jonrobledolopez@gmail.com>
2024-02-28 18:29:34 +0100

    ListView inicial

--- app/src/main/java/com/example/entrega1/MainActivity.java
+++ app/src/main/java/com/example/entrega1/MainActivity.java
@@ -2,14 +2,77 @@ package com.example.entrega1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ArrayList<String> tareas;
+    private ArrayAdapter<String> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tareas = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, tareas);
+
+        ListView listView = findViewById(R.id.tareasListView);
+        listView.setAdapter(adapter);
+
+        Button botonAnadirTarea = findViewById(R.id.botonAnadirTarea);
+        botonAnadirTarea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mostrarDialogo();
+            }
+        });
+
+
+
+    }
+
+
+    private void mostrarDialogo() {
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.anadir_tarea_dialogo, null);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(dialogView)
+                .setTitle("Añadir Tarea")
+                .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        EditText editTextTarea = dialogView.findViewById(R.id.editTextTarea);
+                        String nuevaTarea = editTextTarea.getText().toString();
+                        if (!nuevaTarea.isEmpty()) {
+                            añadirTarea(nuevaTarea);
+                        }
+                    }
+                })
+                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // Cancelar
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void añadirTarea(String nuevaTarea) {
+        tareas.add(nuevaTarea);
+        adapter.notifyDataSetChanged();
     }
 }
