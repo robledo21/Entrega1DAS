4997ca269e8f179059d47dd615bb1534d79c4413
robledo21 <jonrobledolopez@gmail.com>
2024-03-02 19:19:29 +0100

    Notificaciones

--- app/src/main/java/com/example/entrega1/MainActivity.java
+++ app/src/main/java/com/example/entrega1/MainActivity.java
@@ -1,10 +1,21 @@
 package com.example.entrega1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -18,6 +29,7 @@ import android.widget.ListView;
 import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ArrayList<String> tareas;
     private ArrayAdapter<String> adapter;
+    private static final String ID_CANAL = "Canal 1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        crearCanalNotificaciones();
 
     }
 
@@ -115,7 +128,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void añadirTarea(String nuevaTarea, String prioridad) {
-        String tareaConPrioridad = nuevaTarea;
+        String tareaConPrioridad = nuevaTarea + " - Prioridad: " + prioridad;
         tareas.add(tareaConPrioridad);
         adapter.notifyDataSetChanged();
     }
@@ -175,8 +188,71 @@ public class MainActivity extends AppCompatActivity {
 
         // Remueve la tarea de la lista
         if (position != ListView.INVALID_POSITION) {
+            // Obtener la tarea antes de eliminarla
+            String tareaFinalizada = tareas.get(position);
+
+            // Eliminar la tarea de la lista
             tareas.remove(position);
             adapter.notifyDataSetChanged();
+
+            // Mostrar la notificación
+            if (NotificationManagerCompat.from(this).areNotificationsEnabled()) {
+                // Notifications are enabled, proceed with posting notifications
+                mostrarNotificacionFinalizacion(tareaFinalizada);
+            } else {
+                // Notifications are not enabled, request permission or handle accordingly
+                // You can prompt the user to enable notifications or navigate to settings
+                requestPermissions(new String[]{Manifest.permission.POST_NOTIFICATIONS}, 11);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 11)
+        {
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(this, "Notificaciones permitidas", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Notificaciones denegadas", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void mostrarNotificacionFinalizacion(String tareaFinalizada) {
+        // Create an explicit intent for an activity in your app
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, ID_CANAL)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("Felicidades!")
+                .setContentText("Has completado la tarea: " + tareaFinalizada)
+                .setAutoCancel(true)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent)
+                .setFullScreenIntent(pendingIntent, true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        // Notificar
+        notificationManager.notify(1, builder.build());
+    }
+
+    private void crearCanalNotificaciones() {
+        requestPermissions(new String[]{Manifest.permission.POST_NOTIFICATIONS}, 11);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "MiCanal";
+            String description = "Notificaciones App";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(ID_CANAL, name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
         }
     }
 
